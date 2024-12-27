8ad9475be8294f8d61f975368854474d93df86ba
xUnfazedFade <fahanor22@gmail.com>
2024-12-28 01:39:20 +0800

    Update Pus.lua

--- Pus.lua
+++ Pus.lua
@@ -1,103 +1,72 @@
---only skid if u give credit :3
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
-local Window = Rayfield:CreateWindow({
-    Name = "moma hub",
-    Icon = 0,
-    LoadingTitle = "moma hub loading",
-    LoadingSubtitle = "by moma1133222",
-    Theme = "Default",
-    DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false,
-    ConfigurationSaving = {
-       Enabled = false,
-       FolderName = nil,
-       FileName = "moma hub"
-    },
-    Discord = {
-       Enabled = false,
-       Invite = "noinvitelink",
-       RememberJoins = true
-    },
-    KeySystem = false,
-    KeySettings = {
-       Title = "Untitled",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
-       FileName = "Key",
-       SaveKey = true,
-       GrabKeyFromSite = false,
-       Key = {"Hello"}
-    }
+-- Create the GUI Window
+local Window = OrionLib:MakeWindow({
+    Name = "Pull-Up Legends OP GUI (by:Xploiter_RBLX)",
+    HidePremium = false,
+    SaveConfig = false,
+    ConfigFolder = "PullUpLegendsConfig"
 })
 
-local Tab = Window:CreateTab("Main", 4483362458)
-local Section = Tab:CreateSection("Main stuff")
-
-Rayfield:Notify({
-    Title = "Follow me please",
-    Content = "my script blox moma1133222",
-    Duration = 6.5,
-    Image = 4483362458,
+-- Main Tab
+local MainTab = Window:MakeTab({
+    Name = "Main",
+    Icon = "rbxassetid://4483362458",
+    PremiumOnly = false
 })
 
+-- Notify the user
+OrionLib:MakeNotification({
+    Name = "Follow me please",
+    Content = "My script blox moma1133222",
+    Image = "rbxassetid://4483362458",
+    Time = 6.5
+})
+
+-- Remotes
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local remote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("ClaimReward")
+local claimRewardRemote = ReplicatedStorage.Remotes:WaitForChild("ClaimReward")
+local rebirthRemote = ReplicatedStorage.Remotes:WaitForChild("Rebirth")
+local claimDailyRemote = ReplicatedStorage.DailyEvents:WaitForChild("ClaimDaily")
 
-local args = {
-    [1] = "Power",
-    [2] = 0
-}
-
-local Input = Tab:CreateInput({
-    Name = "Give Power(dont go to high or u WILL get kickt)",
-    CurrentValue = "",
-    PlaceholderText = "Give Power",
-    RemoveTextAfterFocusLost = false,
-    Flag = "Input1",
-    Callback = function(Text)
-        args[2] = tonumber(Text) or 300000000
-        print("Input updated to:", args[2])
-        remote:FireServer(unpack(args))
-    end,
+-- Power Input
+MainTab:AddTextbox({
+    Name = "Give Power (Don't go too high!)",
+    Default = "",
+    TextDisappear = true,
+    Callback = function(Value)
+        local args = {
+            [1] = "Power",
+            [2] = tonumber(Value) or 300000000
+        }
+        claimRewardRemote:FireServer(unpack(args))
+        print("Power set to:", args[2])
+    end
 })
 
-remote:FireServer(unpack(args))
-
-local remote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("Rebirth")
-
-local args = {
-    [1] = 0
-}
-
-local Input = Tab:CreateInput({
-    Name = "Give Rebirths(max 1m)",
-    CurrentValue = "",
-    PlaceholderText = "Give rebirths",
-    RemoveTextAfterFocusLost = false,
-    Flag = "RebirthInput",
-    Callback = function(Text)
-        args[1] = tonumber(Text) or 0
-        print("Rebirths updated to:", args[1])
-        remote:InvokeServer(unpack(args))
-    end,
+-- Rebirth Input
+MainTab:AddTextbox({
+    Name = "Give Rebirths (Max 1M)",
+    Default = "",
+    TextDisappear = true,
+    Callback = function(Value)
+        local args = {
+            [1] = tonumber(Value) or 0
+        }
+        rebirthRemote:InvokeServer(unpack(args))
+        print("Rebirths set to:", args[1])
+    end
 })
 
-remote:InvokeServer(unpack(args))
-
-local claimDailyRemote = ReplicatedStorage:WaitForChild("DailyEvents"):WaitForChild("ClaimDaily")
-
+-- Spam Claim Daily Toggle
 local isToggled = false
-
-local Toggle = Tab:CreateToggle({
-    Name = "Spam Claim Daily(op pets)",
-    CurrentValue = false,
-    Flag = "Toggle1",
+MainTab:AddToggle({
+    Name = "Spam Claim Daily (OP Pets)",
+    Default = false,
     Callback = function(Value)
         isToggled = Value
         if isToggled then
-            print("Claim Daily Toggle is ON.")
+            print("Spam Claim Daily ON")
             coroutine.wrap(function()
                 while isToggled do
                     claimDailyRemote:FireServer()
@@ -105,73 +74,49 @@ local Toggle = Tab:CreateToggle({
                 end
             end)()
         else
-            print("Claim Daily Toggle is OFF.")
+            print("Spam Claim Daily OFF")
         end
-    end,
+    end
 })
 
-local Tab = Window:CreateTab("Teleports", 4483362458)
-local Section = Tab:CreateSection("Teleports")
-
-local Players = game:GetService("Players")
-local localPlayer = Players.LocalPlayer
-
-local Button = Tab:CreateButton({
-    Name = "World 1 King of the hill",
-    Callback = function()
-        if localPlayer.Character and localPlayer.Character:FindFirstChild("Humanoid") then
-            localPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(998, 97, 13)
-            print("Teleported to (998, 97, 13).")
-        else
-            print("Character not found or not loaded.")
-        end
-    end,
+-- Teleports Tab
+local TeleportsTab = Window:MakeTab({
+    Name = "Teleports",
+    Icon = "rbxassetid://4483362458",
+    PremiumOnly = false
 })
 
-local Button = Tab:CreateButton({
-    Name = "World 2 King of the hill",
-    Callback = function()
-        if localPlayer.Character and localPlayer.Character:FindFirstChild("Humanoid") then
-            localPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-150, 435, -15985)
-            print("Teleported to (-150, 435, -15985).")
-        else
-            print("Character not found or not loaded.")
+local localPlayer = game.Players.LocalPlayer
+
+-- Add teleport buttons
+local function createTeleportButton(name, cframe)
+    TeleportsTab:AddButton({
+        Name = name,
+        Callback = function()
+            if localPlayer.Character and localPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                localPlayer.Character.HumanoidRootPart.CFrame = cframe
+                print("Teleported to:", cframe)
+            else
+                print("Character not found or not loaded.")
+            end
         end
-    end,
+    })
+end
+
+createTeleportButton("World 1 King of the Hill", CFrame.new(998, 97, 13))
+createTeleportButton("World 2 King of the Hill", CFrame.new(-150, 435, -15985))
+createTeleportButton("World 3 King of the Hill", CFrame.new(8386, 6320, 142))
+createTeleportButton("World 4 King of the Hill", CFrame.new(-7834, 5623, -795))
+createTeleportButton("World 5 King of the Hill", CFrame.new(-3462, 723, -264))
+
+-- Add Credits Section
+local CreditsTab = Window:MakeTab({
+    Name = "Credits",
+    Icon = "rbxassetid://4483362458",
+    PremiumOnly = false
 })
 
-local Button = Tab:CreateButton({
-    Name = "World 3 King of the hill",
-    Callback = function()
-        if localPlayer.Character and localPlayer.Character:FindFirstChild("Humanoid") then
-            localPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(8386, 6320, 142)
-            print("Teleported to (8386, 6320, 142).")
-        else
-            print("Character not found or not loaded.")
-        end
-    end,
-})
+CreditsTab:AddParagraph("Credits", "GUI by: Xploiter_RBLX\nOriginal Script by: moma1133222")
 
-local Button = Tab:CreateButton({
-    Name = "World 4 King of the hill",
-    Callback = function()
-        if localPlayer.Character and localPlayer.Character:FindFirstChild("Humanoid") then
-            localPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7834, 5623, -795)
-            print("Teleported to (-7834, 5623, -795).")
-        else
-            print("Character not found or not loaded.")
-        end
-    end,
-})
-
-local Button = Tab:CreateButton({
-    Name = "World 5 King of the hill",
-    Callback = function()
-        if localPlayer.Character and localPlayer.Character:FindFirstChild("Humanoid") then
-            localPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3462, 723, -264)
-            print("Teleported to (-3462, 723, -264).")
-        else
-            print("Character not found or not loaded.")
-        end
-    end,
-})
+-- Finish the GUI setup
+OrionLib:Init()
